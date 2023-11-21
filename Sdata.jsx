a2039d1c10322a598a4d0af2d404ee534bcd961e
ShukrataMore <147790416+ShukrataMore@users.noreply.github.com>
2023-11-21 18:48:03 +0530

    Add files via upload

---
+++ Sdata.jsx
@@ -0,0 +1,34 @@
+const Sdata=[
+    {
+      imgsrc:"https://th.bing.com/th/id/OIP.CxwQqURL2k2OI-C-u6nEJAHaEK?pid=ImgDet&rs=1",
+      title:"A NetFlix Series" ,
+      sname:"DARK",
+      link:"https://www.bing.com/videos/search?q=dark+series+img&&view=detail&mid=885DF856BD6FE80F2C03885DF856BD6FE80F2C03&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3Ddark%2Bseries%2Bimg%26FORM%3DHDRSC4"
+    },
+    {
+        imgsrc:"https://th.bing.com/th/id/OIP.saCJ0UOGHdr8qPUnjLLHnAHaK9?pid=ImgDet&rs=1",
+   title:"A NetFlix Series",
+   sname:"STRANGER THINGS",
+   link:"https://www.bing.com/videos/search?q=stranger+things+series+img&&view=detail&mid=DC590A2A232389DCF47BDC590A2A232389DCF47B&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3Dstranger%2Bthings%2Bseries%2Bimg%26FORM%3DHDRSC4"
+    },
+    {
+        imgsrc:"https://wallpaperaccess.com/full/3034964.png",
+   title:"A NetFlix Series",
+   sname:"MONEY HEIST" ,
+   link:"https://www.bing.com/videos/search?q=money+heist+series+img&&view=detail&mid=61D43EFD9EAA5A3D924661D43EFD9EAA5A3D9246&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3Dmoney%2Bheist%2Bseries%2Bimg%26FORM%3DHDRSC4"
+    },
+    {
+      imgsrc:"https://th.bing.com/th/id/OIP.sLurL3uDjAYtUvUrFWQ0YgHaEK?pid=ImgDet&rs=1",
+ title:"A NetFlix Series",
+ sname:"WEDNESDAY" ,
+ link:"https://www.bing.com/videos/search?q=wednesday+series+netflix&&view=detail&mid=313B3876F54AB63DE7B5313B3876F54AB63DE7B5&&FORM=VRDGAR&ru=%2Fvideos%2Fsearch%3Fq%3Dwednesday%2Bseries%2Bnetflix%26FORM%3DHDRSC4"
+  },
+  {
+    imgsrc:"https://th.bing.com/th/id/OIP.3vPcRQ4brHwKBGh4t9ekEAHaEK?pid=ImgDet&rs=1",
+title:"A NetFlix Series",
+sname:"ALL OF US ARE DEAD" ,
+link:"https://www.bing.com/videos/search?q=is+all+of+us+are+dead+netflix+series&&view=detail&mid=E572242700E927B38E6CE572242700E927B38E6C&&FORM=VRDGAR"
+},
+];
+
+export default Sdata;
